53eb5e196e54c0f766ae160d3f1e92fed109dda9
MUKESH2215 <mukeshkannaysj266@gmail.com>
2025-06-05 15:40:14 +0530

    Time: 1 ms (99.67%), Space: 42.2 MB (44.8%) - LeetHub

---
+++ 3379-score-of-a-string/3379-score-of-a-string.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int scoreOfString(String s) {
+        int score = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            score += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+        return score;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.scoreOfString("hello"));  // Output: 13
+        System.out.println(sol.scoreOfString("zaz"));    // Output: 50
+    }
+}
+
