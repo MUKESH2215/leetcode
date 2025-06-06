d77ed0bb052c02aa71f6742664549e79f48c42d1
MUKESH2215 <mukeshkannaysj266@gmail.com>
2025-06-06 15:42:40 +0530

    Time: 367 ms (79.29%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-SELECT email AS Email
-FROM Person
-GROUP BY email
-HAVING COUNT(*) > 1;
+SELECT 
+    email as Email 
+FROM Person 
+GROUP BY email 
+HAVING COUNT(email) > 1;
