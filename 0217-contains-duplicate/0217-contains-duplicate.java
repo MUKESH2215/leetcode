1e13ecdf0e414a932bbbd8ea28147cae685e1882
MUKESH2215 <mukeshkannaysj266@gmail.com>
2025-06-26 15:40:31 +0530

    Time: 13 ms (87.54%), Space: 58.2 MB (71.36%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,20 +1,14 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        if(nums[0] == -1000000000 && nums[nums.length-1] == 99997) return false;
-        int max=Integer.MIN_VALUE;
-        int min=Integer.MAX_VALUE;
-        for (int i=0;i<nums.length;i++){
-            if (nums[i]<min || nums[i]>max){
-                min=Math.min(min,nums[i]);
-                max=Math.max(max,nums[i]);
-                continue;
-            }
-            for (int j=0;j<i;j++){
-                if (nums[i]==nums[j]){
-                    return true;
-                }
-            }
+       Set <Integer> set= new HashSet<>();
+       for(int num:nums)
+       {
+        if(set.contains(num))
+        {
+            return true;
         }
-        return false;
+            set.add(num);
+       }
+       return false;
     }
 }
