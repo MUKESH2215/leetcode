a25f796f8085a514549568b5594244a615a00a1a
MUKESH2215 <mukeshkannaysj266@gmail.com>
2025-06-28 15:41:18 +0530

    Time: 0 ms (100%), Space: 40.7 MB (30.89%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,15 +1,24 @@
+// class Solution {
+//     public boolean checkPerfectNumber(int num) {
+//         int sum=0;
+//         for(int i=1;i<num;i++)
+//         {
+//             if(num%i==0)
+//             {
+//                 sum+=i;
+//             }
+//         }
+//         if(sum==num)
+//         {
+//             return true;
+//         }
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int sum=0;
-        for(int i=1;i<num;i++)
-        {
-            if(num%i==0)
-            {
-                sum+=i;
-            }
-        }
-        if(sum==num)
-        {
+        if(num==6 || num==28 || num==496 || num==8128 || num==33550336){
             return true;
         }
         return false;
