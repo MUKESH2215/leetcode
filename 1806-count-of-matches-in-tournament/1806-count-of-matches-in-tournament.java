6418625fd61884e47bd110ce4ec3e8a4d7be2f09
MUKESH2215 <mukeshkannaysj266@gmail.com>
2025-07-15 14:36:10 +0530

    Time: 0 ms (100%), Space: 40.6 MB (40.14%) - LeetHub

--- 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.java
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.java
@@ -1,26 +1,31 @@
 class Solution {
     public int numberOfMatches(int n) {
        
-        int match=0;
-        int adv=0;
+        // int match=0;
+        // int adv=0;
         
+        // while(n!=1)
+        // {
+        //     if(n%2==0)
+        //     {
+        //     match+=Math.floor(n/2);
+        //     adv=(int)Math.ceil(n/2);
+           
+        //     }
+        //     else
+        //     {
+        //        match+=Math.floor((n-1)/2);
+        //         adv=(int)Math.ceil(((n-1)/2)+1);
+           
+        //     }
+        //      n=adv;
+        // }
+        int sum=0;
         while(n!=1)
         {
-            if(n%2==0)
-            {
-            match+=Math.floor(n/2);
-            adv=(int)Math.ceil(n/2);
-           
-            }
-            else
-            {
-               match+=Math.floor((n-1)/2);
-                adv=(int)Math.ceil(((n-1)/2)+1);
-           
-            }
-             n=adv;
+            sum+=n/2;
+            n-=n/2;
         }
-        
-        return match;
+        return sum;
     }
 }
