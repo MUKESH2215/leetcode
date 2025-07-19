65c7075650604b71c26d08e90797943d18c0c341
MUKESH2215 <mukeshkannaysj266@gmail.com>
2025-07-19 09:48:26 +0530

    Time: 1 ms (100%), Space: 58.1 MB (85.12%) - LeetHub

--- 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.java
+++ 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.java
@@ -10,29 +10,46 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        int count=0;
-        int count1=0;
-        ListNode tptr=head;
+        // int count=0;
+        // int count1=0;
+        // ListNode tptr=head;
         if(head==null || head.next==null) return head;
-        while(tptr!=null)
-        {
-            count++;
-            tptr=tptr.next;
-        }
-          tptr=head;
+        // while(tptr!=null)
+        // {
+        //     count++;
+        //     tptr=tptr.next;
+        // }
+        //   tptr=head;
+        // for(int i=0;i<k-1;i++)
+        // {
+        //     tptr=tptr.next;
+        // }
+        // ListNode trtv=head;
+        // for(int i=0;i<count-k;i++)
+        // {
+        //     trtv=trtv.next;
+        // }
+        
+        // int temp=tptr.val;
+        // tptr.val=trtv.val;
+        // trtv.val=temp;
+        // return head;
+
+        ListNode front=head;
+        ListNode end=head;
         for(int i=0;i<k-1;i++)
         {
-            tptr=tptr.next;
+            front=front.next;
         }
-        ListNode trtv=head;
-        for(int i=0;i<count-k;i++)
+        ListNode temp=front;
+        while(temp.next!=null)
         {
-            trtv=trtv.next;
+            end=end.next;
+            temp=temp.next;
         }
-        
-        int temp=tptr.val;
-        tptr.val=trtv.val;
-        trtv.val=temp;
+        int t=end.val;
+        end.val=front.val;
+        front.val=t;
         return head;
     }
 }
