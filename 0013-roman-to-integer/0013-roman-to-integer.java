66a5531fe3caff8b8b8f9b377a07daf60be6e334
Mukesh Kanna Y <mukeshkannaysj266@gmail.com>
2026-02-12 14:55:05 +0530

    Time: 5 ms (34.21%), Space: 47.1 MB (14.82%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,35 @@
+class Solution 
+{
+    public int romanToInt(String s) 
+    {
+       HashMap<Character,Integer>hm=new HashMap<>();
+       hm.put('I',1);
+       hm.put('V',5);
+       hm.put('X',10);
+       hm.put('L',50);
+       hm.put('C',100);
+       hm.put('D',500);
+       hm.put('M',1000);
+       int res=hm.get(s.charAt(s.length()-1));
+       for(int i=s.length()-2;i>=0;i--)
+       {
+        if(hm.get(s.charAt(i))<hm.get(s.charAt(i+1)))
+        {
+            res-=hm.get(s.charAt(i));
+        }
+        else
+        {
+            res+=hm.get(s.charAt(i));
+        }
+       }
+       return res;
+    }
+
+    // public static void main(String[] args) 
+    // {
+    //     Solution solution = new Solution(); 
+    //     String roman = "MCMXCIV";
+    //     System.out.println("The integer value of " + roman + " is: " + solution.romanToInt(roman));
+    // }
+}
+
